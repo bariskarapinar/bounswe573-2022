7a7959ea5df49ae5fcd62f3c83067d2e20dca9eb
bariskarapinar <baris.karapinar@tokeninc.com>
2022-03-10 20:56:36 +0300

    In Progress

--- django-env/bin/activate.fish
+++ django-env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/bariskarapinar/BOUN/djangogirls/django-env"
+set -gx VIRTUAL_ENV "/Users/bariskarapinar/PycharmProjects/bounswe573-2022/django-env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- myvenv/bin/activate.fish
+++ myvenv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/bariskarapinar/BOUN/djangogirls/myvenv"
+set -gx VIRTUAL_ENV "/Users/bariskarapinar/PycharmProjects/bounswe573-2022/myvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH

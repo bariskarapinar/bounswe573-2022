66760ac966228a7bc3591163a661ed4103a3cd53
bariskarapinar <baris.karapinar@tokeninc.com>
2022-05-09 20:24:12 +0300

    Online Learning Platform

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0a(p*c-6(+_+94)4hit666rna@5w#521*q5qgyw+d!ai)0)ju+'
+SECRET_KEY = 'django-insecure-4at#bz7#nbnb+d5*i1%qjy903^wira=v!vym^7pb@^(&*o#@7z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
